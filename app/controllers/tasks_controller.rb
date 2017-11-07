1184fdff357a50372ba4c1e03c30e90e8c964b20
Ryan Wiens <ryaninshanghai@gmail.com>
2017-11-07 15:03:01 +0100

    added routes views and models

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
 
@@ -13,20 +15,43 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
-    @task.save
+   @task = Task.new(task_params)
+   @task.save
 
-    redirect_to restaurant_path(@restaurant)
-  end
+   redirect_to task_path(@task)
+ end
 
-  def edit
+ def edit
 
-  end
+ end
 
-  def update
-  end
+ def update
+  @task.update(task_params) #  name: "Make Dinner", completed: "true"
 
-  def destroy
+    redirect_to task_path(@task)
+ end
+
+ def destroy
+   @task.destroy
+
+    redirect_to tasks_path
+ end
+
+private
+
+ def set_task
+  @task = Task.find(params[:id])
+ end
+
+ def task_params
+    # params
+    # => { id: '1', restaurant: { name: "Loetjes", address: "Amsterdam" } }
+
+    # params.require(:restaurant)
+    # => { name: "Loetjes", address: "Amsterdam", featured: true }
+
+    params.require(:task).permit(:name, :completed)
+    # => { name: "Loetjes", address: "Amsterdam" }
   end
 end
 
